08ff2e280e771a8399f45113608b5dfa77abaa58
wkoroy <wkoroy@gmail.com>
2019-05-18 12:20:41 +0300

    fix default programmatically values

--- app/src/main/java/ru/smartinterestapps/android/vanilla/PrefDefaults.java
+++ app/src/main/java/ru/smartinterestapps/android/vanilla/PrefDefaults.java
@@ -32,10 +32,11 @@ public class PrefDefaults {
 	public static final String  DEFAULT_PLAYLIST_ACTION = "0";
 	public static final boolean COVERLOADER_ANDROID = true;
 
-	public  static final boolean PROXIMITY_SWITCH_TRACK = false;
+	public  static final boolean PROXIMITY_SWITCH_TRACK = true;
 	public static final boolean VOLUME_SWITCH_TRACK = true;
 	public static final boolean USE_IDLE_NOACTIVE_TIMEOUT = false;
 	public static final int     IDLE_NOACTIVE_TIMEOUT = 3600;
+	public static final boolean USE_BOKMARKS_POSITION = false;
 
 	public static final boolean COVERLOADER_VANILLA = true;
 	public static final boolean COVERLOADER_SHADOW = true;
